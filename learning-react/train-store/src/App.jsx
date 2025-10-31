cb2838f0eacb30a9e610145019b40b38cad6a69a
José Ríos <rdjl5791@gmail.com>
2025-10-31 04:52:06 -0600

    A new locomotive model and its corresponding image have been added to the data set.

--- learning-react/train-store/src/App.jsx
+++ learning-react/train-store/src/App.jsx
@@ -5,14 +5,15 @@ import './App.css'
 import loco1 from './assets/images/HO-GP40-UP.png';
 import loco2 from './assets/images/HO-F7-SF.png';
 import loco3 from './assets/images/HO-MALLARD-BR.png';
-import loco4 from './assets/images/HO-GP38-CP.png';  
+import loco4 from './assets/images/HO-GP38-CP.png'; 
+import loco5 from './assets/images/HO-SD40-FERROMEX.png'; 
 
 import locomotives from './data/locomotives.json';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  let locos =[loco1, loco2, loco3, loco4];
+  let locos =[loco1, loco2, loco3, loco4, loco5];
 
   return (
     <>
