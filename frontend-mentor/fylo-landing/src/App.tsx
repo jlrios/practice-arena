ba034bd23262e390e9fdf7601bd715dac0875fec
JLRD <rdjl5791@gmail.com>
2025-11-07 03:47:52 -0600

    Proyecto Fylo Landing Page, para el aprendizaje de React con tailwindcss.

---
+++ frontend-mentor/fylo-landing/src/App.tsx
@@ -0,0 +1,18 @@
+import { Header, Hero } from "./components"
+
+function App() {
+  return (
+    <main className="w-[375px] m-auto">
+      <section className="bg-Navy-850 p-6 h-[420px] relative">
+        <Header />
+        <img className="relative z-10 mt-12" src="/images/illustration-intro.png" alt="Intro illustration." />
+        <img className="absolute bottom-0 left-0" src="/images/bg-curvy-mobile.svg" alt="Curvy mobile." />
+      </section>
+      <section className="bg-Navy-900 p-6">
+        <Hero />
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ frontend-mentor/fylo-landing/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <ul className="flex gap-6">
+          <li><a className="hover:text-Teal-200" href="#">Features</a></li>
+          <li><a className="hover:text-Teal-200" href="#">Team</a></li>
+          <li><a className="hover:text-Teal-200" href="#">Sing In</a></li>
+        </ul>
+      </nav>
+  )
+}
